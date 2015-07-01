d139b90d362e3deaeb22b9655ff7e32dda98bac7
lironko <lironbackup@gmail.com>
2015-07-01 01:42:25 +0000

    some validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ActiveRecord::Base
-  validates :name, uniqueness: { case_sensetive: false }
+  validates :name, uniqueness: { case_sensetive: false }\
+  
   has_many :exercise_records
   has_many :exercise_sets, through: :exercise_records
 end
--- app/models/exercise_record.rb
+++ app/models/exercise_record.rb
@@ -1,8 +1,9 @@
 class ExerciseRecord < ActiveRecord::Base
-  has_many :exercise_sets
-  belongs_to :exercise
   validates :exercise_id, inclusion: { in: Exercise.pluck(:id),
-    message: "%{value} please pick an exercise from the list." }
+  message: " please pick an exercise from the exercises list." }
+  
+  has_many :exercise_sets, dependent: :destroy
+  belongs_to :exercise
   accepts_nested_attributes_for :exercise_sets, :reject_if => :all_blank, :allow_destroy => true
   
 end
