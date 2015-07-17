3647f193dfba94f57a5ad1a0c17f7226cd6c1b54
lironko <lironbackup@gmail.com>
2015-07-17 04:11:12 +0000

    exercises in workout index

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,12 @@ class Workout < ActiveRecord::Base
   
   belongs_to :user
   has_many :exercise_records, dependent: :destroy
+  has_many :exercise_sets, through: :exercise_records
   accepts_nested_attributes_for :exercise_records, :reject_if => :all_blank, :allow_destroy => true
+  
+  #exercises included in the workout (string)
+  def included_exercises
+    ids = self.exercise_records.map &:exercise_id
+    (Exercise.where(id: ids).map &:name).join(', ')
+  end
 end
