19c1271c98fd6d1c258d6e0b64be8fe7d4e6faa0
lironko <lironbackup@gmail.com>
2015-07-19 23:31:07 +0000

    made them pretty

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,7 @@ class Exercise < ActiveRecord::Base
   has_many :exercise_records
   has_many :exercise_sets, through: :exercise_records
   
+  def average_weight
+    self.exercise_sets.average(:weight)
+  end
 end
