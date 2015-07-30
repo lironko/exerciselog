6c14ae86241c3568a20baed2e3a42bedf594af05
lironko <lironbackup@gmail.com>
2015-07-30 00:42:30 +0000

    static changes

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,6 @@
 class ExercisesController < ApplicationController
   before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /exercises
   # GET /exercises.json
